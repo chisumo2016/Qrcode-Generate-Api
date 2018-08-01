bb6315144c88c46d35bed6f4178b6b0ea82c92f6
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-08-01 12:35:15 +0100

    Add one missing field payment_method  in transactions migration

--- database/migrations/2018_07_31_195158_create_transactions_table.php
+++ database/migrations/2018_07_31_195158_create_transactions_table.php
@@ -18,6 +18,7 @@ class CreateTransactionsTable extends Migration
             $table->integer('user_id'); //User making transaction
             $table->integer('qrcode_id');
             $table->integer('qrcode_owner_id')->nullable();  // track the qrcode
+            $table->string('payment_method')->nullable();//paypal ,stripe ,paystack
             $table->longText('message')->nullable();  //msg comes from website
             $table->float('amount',10,4);
             $table->string('status')->default('initiated'); // Initiated, completed and payment failed
