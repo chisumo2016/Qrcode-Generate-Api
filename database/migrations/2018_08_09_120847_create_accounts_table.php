bede9f0b47c183d016d8107063ed4385be7acc99
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-08-09 14:42:00 +0100

    Created a migrations for account and account_histories including the fields name

---
+++ database/migrations/2018_08_09_120847_create_accounts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->float('balance', 10, 4)->default(0);
+            $table->float('total_credit', 10,4)->default(0);
+            $table->float('total_debit', 10,4)->default(0);
+            $table->string('withdraw_method')->default('bank');
+            $table->string('payment_email')->nullable();
+            $table->string('bank_name')->nullable();
+            $table->string('bank_branch')->nullable();
+            $table->string('bank_account')->nullable();
+            $table->string('country')->nullable();
+            $table->longText('other_details')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
