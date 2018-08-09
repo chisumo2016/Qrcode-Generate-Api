5523c8e668bb1b526d20631acef17186e6ecf6c5
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-08-09 10:34:42 +0100

    Organising menus according to user roles

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,16 +1,26 @@
-<li class="{{ Request::is('qrcodes*') ? 'active' : '' }}">
-    <a href="{!! route('qrcodes.index') !!}"><i class="fa fa-edit"></i><span>Qrcodes</span></a>
-</li>
-
-<li class="{{ Request::is('roles*') ? 'active' : '' }}">
-    <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
-</li>
+{{-- All User --}}
 
 <li class="{{ Request::is('trasanctions*') ? 'active' : '' }}">
     <a href="{!! route('trasanctions.index') !!}"><i class="fa fa-edit"></i><span>Trasanctions</span></a>
 </li>
+@if(Auth::user()->role_id < 4)
+{{-- All Webmaster --}}
+<li class="{{ Request::is('qrcodes*') ? 'active' : '' }}">
+    <a href="{!! route('qrcodes.index') !!}"><i class="fa fa-edit"></i><span>Qrcodes</span></a>
+</li>
+@endif
 
+{{-- All Moderators--}}
+@if(Auth::user()->role_id < 3)
 <li class="{{ Request::is('users*') ? 'active' : '' }}">
     <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
 </li>
+@endif
+{{-- All Admin --}}
+@if(Auth::user()->role_id == 1)
+<li class="{{ Request::is('roles*') ? 'active' : '' }}">
+    <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
+</li>
+
+@endif
 
