57a9bb6002a6c93ecd06a23bf5ac03965b0ef11d
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-09-11 20:02:48 +0100

    Split the transaction into two parts

--- resources/views/trasanctions/show_fields.blade.php
+++ resources/views/trasanctions/show_fields.blade.php
@@ -1,89 +1,97 @@
-<!-- Id Field -->
+<div class="col-md-6">
+    <!-- Id Field -->
 {{--<div class="form-group">--}}
-    {{--{!! Form::label('id', 'ID:') !!}--}}
-    {{--<p>Transaction ID: #{!! $trasanction->id !!}</p>--}}
+{{--{!! Form::label('id', 'ID:') !!}--}}
+{{--<p>Transaction ID: #{!! $trasanction->id !!}</p>--}}
 {{--</div>--}}
 
 <!-- Qrcode Id Field -->
-<div class="form-group">
-    {!! Form::label('qrcode_id', 'Product Name:') !!}
+    <div class="form-group">
+        {!! Form::label('qrcode_id', 'Product Name:') !!}
 
-    <p>
-        <a href="/qrcodes/{!! $trasanction->qrcode['id'] !!}">
-           <b> {!! $trasanction->qrcode['product_name'] !!} </b>
-        </a>
+        <p>
+            <a href="/qrcodes/{!! $trasanction->qrcode['id'] !!}">
+                <b> {!! $trasanction->qrcode['product_name'] !!} </b>
+            </a>
 
-    </p>
-</div>
+        </p>
+    </div>
 
-<!-- Amount Field -->
-<div class="form-group">
-    {!! Form::label('amount', 'Amount:') !!}
-    <p>£ {!! $trasanction->amount !!}</p>
-</div>
+    <!-- Amount Field -->
+    <div class="form-group">
+        {!! Form::label('amount', 'Amount:') !!}
+        <p>£ {!! $trasanction->amount !!}</p>
+    </div>
 
-<!-- Amount Field -->
-@if($trasanction->drcode['product_url'])
-<div class="form-group">
-    {{--{!! Form::label('amount', 'Amount:') !!}--}}
-    <p><a href="{!! $trasanction->drcode['product_url'] !!}" class="btn btn-success btn-lg">Return to mechant side </a></p>
-</div>
-@endif
+    <!-- Amount Field -->
+    @if($trasanction->qrcode['product_url'])
+        <div class="form-group">
+            {{--{!! Form::label('amount', 'Amount:') !!}--}}
+            <p><a href="{!! $trasanction->drcode['product_url'] !!}" class="btn btn-success btn-lg">Return to mechant side </a></p>
+        </div>
+    @endif
 
-<!-- User Id Field -->
-<div class="form-group">
-    {!! Form::label('user_id', 'Buyer Name:') !!}
-
-    <p>
-        <a href="/users/{!! $trasanction->user['id'] !!}">
-        {!! $trasanction->user['name'] !!}  | {!! $trasanction->user['email'] !!}
-        </a>
-    </p>
 </div>
 
 
+<div class="col-md-6">
+    <!-- Buyer Field -->
+    <div class="form-group">
+        {!! Form::label('user_id', 'Buyer Name:') !!}
 
-<!-- Qrcode Owner Id Field -->
-<div class="form-group">
-    {!! Form::label('qrcode_owner_id', 'Qrcode Owner Mame:') !!}
-    <p>
-        <a href="/users/{!! $trasanction->qrcode_owner['id'] !!}">
+        <p>
+            <a href="/users/{!! $trasanction->user['id'] !!}">
+                {!! $trasanction->user['name'] !!}  | {!! $trasanction->user['email'] !!}
+            </a>
+        </p>
+    </div>
 
-            {!! $trasanction->qrcode_owner['name']  !!}
-        </a>
 
-    </p>
+
+    <!-- Qrcode Owner Id Field -->
+    <div class="form-group">
+        {!! Form::label('qrcode_owner_id', 'Qrcode Owner Mame:') !!}
+        <p>
+            <a href="/users/{!! $trasanction->qrcode_owner['id'] !!}">
+
+                {!! $trasanction->qrcode_owner['name']  !!}
+            </a>
+
+        </p>
+    </div>
+
+    <!-- Payment Method Field -->
+    <div class="form-group">
+        {!! Form::label('payment_method', 'Payment Method:') !!}
+        <p>{!! $trasanction->payment_method !!}</p>
+    </div>
+
+    <!-- Message Field -->
+    <div class="form-group">
+        {!! Form::label('message', 'Message:') !!}
+        <p>{!! $trasanction->message !!}</p>
+    </div>
+
+
+
+    <!-- Status Field -->
+    <div class="form-group">
+        {!! Form::label('status', 'Status:') !!}
+        <p>{!! $trasanction->status !!}</p>
+    </div>
+
+    <!-- Created At Field -->
+    <div class="form-group">
+        {!! Form::label('created_at', 'Created At:') !!}
+        <p>{!! $trasanction->created_at->format('D d, M, Y h:i') !!}</p>
+    </div>
+
+    <!-- Updated At Field -->
+    <div class="form-group">
+        {!! Form::label('updated_at', 'Updated At:') !!}
+        <p>{!! $trasanction->updated_at->format('D d, M, Y h:i') !!}</p>
+    </div>
+
+
+
 </div>
-
-<!-- Payment Method Field -->
-<div class="form-group">
-    {!! Form::label('payment_method', 'Payment Method:') !!}
-    <p>{!! $trasanction->payment_method !!}</p>
-</div>
-
-<!-- Message Field -->
-<div class="form-group">
-    {!! Form::label('message', 'Message:') !!}
-    <p>{!! $trasanction->message !!}</p>
-</div>
-
-
-
-<!-- Status Field -->
-<div class="form-group">
-    {!! Form::label('status', 'Status:') !!}
-    <p>{!! $trasanction->status !!}</p>
-</div>
-
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{!! $trasanction->created_at->format('D d, M, Y h:i') !!}</p>
-</div>
-
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{!! $trasanction->updated_at->format('D d, M, Y h:i') !!}</p>
-</div>
-
