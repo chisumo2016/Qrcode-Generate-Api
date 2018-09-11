41cd872e6d820e0e18c382faf695c16be5cc74c7
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-09-11 10:17:01 +0100

    Add payout Button in account

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="content-header">
-        <h1>
+        <h1 class="pull-left">
             Account: {{ $account->id }}
 
             <small>
@@ -12,7 +12,26 @@
             </small>
         </h1>
     </section>
+
+    <h1 class="pull-right">
+        {{--<a href="" class="btn btn-primary">Apply for payout</a>--}}
+        {!! Form::open(['route' => ['users.destroy', $account->id], 'method' => 'delete']) !!}
+        {!! Form::button('<i class="glyphicon glyphicon-trash"></i> Apply for payout', ['type' => 'submit', 'class' => 'btn btn-primary', 'onclick' => "return confirm('Are you sure?')"]) !!}
+
+        {!! Form::close() !!}
+
+
+        {!! Form::open(['route' => ['users.destroy', $account->id], 'method' => 'delete']) !!}
+        {!! Form::button('<i class="glyphicon glyphicon-trash"></i> Mark As Paid', ['type' => 'submit', 'class' => 'btn btn-primary ', 'onclick' => "return confirm('Are you sure?')"]) !!}
+
+        {!! Form::close() !!}
+    </h1>
+
+
+
+
     <div class="content">
+        <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
