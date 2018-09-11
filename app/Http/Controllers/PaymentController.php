48f1acf86f0c70caa2b850af7a54bab4c8e21513
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-09-11 17:34:36 +0100

    Update owner account

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -45,12 +46,27 @@ class PaymentController extends Controller
         dd($paymentDetails);
         //check if the amount paid is same as amount they are supposed to pay
 
-        $qrcode = QrcodeModel::find($paymentDetails['data']['metadata']['qrcode_id'] );
+        $qrcode = QrcodeModel::find($paymentDetails['data']['metadata']['qrcode_id']);
         if($qrcode->amount !=  ($paymentDetails['data']['amount'] / 100 ))
         {
             Flash::error('Sorry,you paid the wrong amount. pLeae conta');
             return redirect()->route('qrcodes.show',['id'=> $paymentDetails['data']['metadata']['qrcode_id'] ]);
         }
+        //
+        //Update the transaction
+        Trasanction::where('id', $paymentDetails['data']['metadata']['transaction_id'] )->update(['status'=>'completed']);
+
+        //update the qrcode owner account  and account history
+        $qrCodeOwnerAccount = Account::where('user_id', $qrcode->user_id)->first();
+        Account::where('user_id', $qrcode->user_id)->update([
+            'balance'           =>  ($qrCodeOwnerAccount->balance +  $qrcode->amount),
+            'total_credit'      =>  ($qrCodeOwnerAccount->balance +  $qrcode->amount)
+        ]);
+
+        
+        //update buyer account and account history
+        //send email alert both parties
+        //send sms
         // Now you have the payment details,
         // you can store the authorization_code in your db to allow for recurrent subscriptions
         // you can then redirect or do whatever you want
