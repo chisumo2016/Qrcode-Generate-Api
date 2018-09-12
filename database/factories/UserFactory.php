c61436d989d9b5a511c989db236ef2ade0a36575
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-09-12 13:49:36 +0100

    Roles Seeder created successfully

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,12 +12,15 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
-
+$passwordHash = Hash::make('secret');
+$rememberToken =  str_random(10);
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        //'role_id'=>$faker->numberBetween(1,4),
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'password' => $passwordHash, // secret
+        'role_id'   => rand(1,4),
+        'remember_token' =>$rememberToken,
     ];
 });
---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Role::create([
+            'name' => 'Admin'
+        ]);
+
+        Role::create([
+            'name' => 'Moderator'
+        ]);
+
+
+        Role::create([
+            'name' => 'Webmaster'
+        ]);
+
+
+        Role::create([
+            'name' => 'Buyer'
+        ]);
+    }
+}
